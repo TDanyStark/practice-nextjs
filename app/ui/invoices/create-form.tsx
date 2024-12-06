79a6c5b4adf77fe7fce44bb371f88468bd36375e
Daniel Amado <daniel.amadove@gmail.com>
2024-12-06 17:47:33 -0500

    Eliminar consola de depuración en el formulario de creación de facturas

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -19,7 +19,6 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
     values: {} 
   };
   const [state, formAction] = useActionState(createInvoice, initialState);
-  console.log(state);
   return (
     <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
