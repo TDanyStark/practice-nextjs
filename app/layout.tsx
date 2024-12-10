74e447cb406eb71541df4799cab703ea568c0ab5
Daniel Amado <daniel.amadove@gmail.com>
2024-12-10 11:42:59 -0500

    Agregar metadatos de página para el dashboard, clientes, creación y edición de facturas, y página de inicio de sesión

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
@@ -8,16 +18,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        />
-        <title>My App</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="/fonts/fonts.css" />
-      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,26 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
+// import { getToken } from 'next-auth/jwt';
+// import { NextRequest, NextResponse } from 'next/server';
+
+// 1. Use middleware directly
+export const { auth: middleware } = NextAuth(authConfig)
  
-export default NextAuth(authConfig).auth;
- 
+// 2. Wrapped middleware option
+// const { auth } = NextAuth(authConfig)
+
+// export default auth(async function middleware(req: NextRequest) {
+//   // Obtenemos el token para verificar si el usuario está autenticado
+//   const token = await getToken({ req, secret: process.env.AUTH_SECRET })
+
+//   // Ejemplo: Si la ruta es /dashboard, verificar si existe token (usuario autenticado)
+//   if (req.nextUrl.pathname.startsWith('/dashboard') && !token) {
+//     return NextResponse.redirect(new URL('/login', req.url))
+//   }
+
+//   return NextResponse.next()
+// })
+
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
